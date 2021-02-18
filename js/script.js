380e57294e3a48349afe14d988332bc8031e0d79
mirco verderosa <verderosamircowork@gmail.com>
2021-02-18 17:19:59 +0100

    aggiunto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 new Vue({
-  el:'project',
-  data:{},
+  el:'#project',
+  data:{
+
+ // oggetto in cui ci sono i dati dell'header
+ // con possibilità di espanderlo successivamente
+
+   HeadMenu:[
+     {
+        name:'Home',
+        content:'',
+      },
+     {
+        name:'Pages',
+        content:'',
+      },
+     {
+        name:'Courses',
+        content:'',
+      },
+     {
+        name:'Features',
+        content:'',
+      },
+     {
+        name:'Blog',
+        content:'',
+      },
+     {
+        name:'Shop',
+        content:'',
+      },
+
+      ],
+
+
+  },
 })
 
 Vue.config.devtools = true;
